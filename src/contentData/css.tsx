cc01a2012771f3a771fba1ab22d05877cbaaef2a
rahi2109 <rahi.anshuman2109@gmail.com>
2025-06-28 01:26:34 +0530

    fiex error

--- src/contentData/css.tsx
+++ src/contentData/css.tsx
@@ -7,37 +7,37 @@
 
 }
 {
-    Types of selector in CSS
-    1. Simple Selector
-        a. Element Selector - e.g body, p, div etc
-        b. Class Selector - e.g .class1, .class2 etc
-        c. Id Selector - e.g #id1, #id2
-    2. Psudo Selector - button:hover
-    3. Multiple Selector - table, tr, td {}
-    4. Universal Selector - *
-    5. Nested Selector - div > a (put css protery for a tag which is inside div) 
-    6. Attribute Selector - a[targer] - <a href = "www.google.com" target = "do_something">Some text is here</a> 
+//     Types of selector in CSS
+//     1. Simple Selector
+//         a. Element Selector - e.g body, p, div etc
+//         b. Class Selector - e.g .class1, .class2 etc
+//         c. Id Selector - e.g #id1, #id2
+//     2. Psudo Selector - button:hover
+//     3. Multiple Selector - table, tr, td {}
+//     4. Universal Selector - *
+//     5. Nested Selector - div > a (put css protery for a tag which is inside div) 
+//     6. Attribute Selector - a[targer] - <a href = "www.google.com" target = "do_something">Some text is here</a> 
 
-    specificity
+//     specificity
 
-        tag selector < class selector <id selector < inline < !IMP
-
-
-
-    Box module - Every element on page is a rectangular box which may have Width, height, padding, border and margin.
-
-    margin - space between two div.
-    padding space between content and border.
+//         tag selector < class selector <id selector < inline < !IMP
 
 
-    units in CSS
 
-    px - It is 1/96 inch. It is the smallest unit of the screen. It is fixed for all screen.
-    %unit - It varies based on parent component. 
+//     Box module - Every element on page is a rectangular box which may have Width, height, padding, border and margin.
+
+//     margin - space between two div.
+//     padding space between content and border.
+
+
+//     units in CSS
+
+//     px - It is 1/96 inch. It is the smallest unit of the screen. It is fixed for all screen.
+//     %unit - It varies based on parent component. 
     
-    Font Size unit
-        - em - relative to parent.
-        - rem - relative to root i.e html 
+//     Font Size unit
+//         - em - relative to parent.
+//         - rem - relative to root i.e html 
     
-}
+// }
 
--- src/utility/cssFile.tsx
+++
@@ -1,7 +0,0 @@
-
-import courses from '../static/css/courses.css';
-
-
-export  {courses};
-
-// import.meta.env.VITE_Imge
